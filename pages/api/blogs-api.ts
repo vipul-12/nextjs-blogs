449e73abebcc96612a993e67cf3c1818143e16af
vipul-1902 <vipul_shinde2@persistent.com>
2024-02-06 16:12:32 +0530

    api

--- pages/api/blogs-api.ts
+++ pages/api/blogs-api.ts
@@ -1,8 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import * as fs from "fs";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  fs.readdir("blogData", (error, data) => {
-    res.status(200).json(data);
-  });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    let blogs: Object[] = [];
+    let files = await fs.promises.readdir("blogData");
+    for (let i = 0; i < files.length; i++) {
+      const item = files[i];
+      let blogContent = await fs.promises.readFile(`blogData/${item}`, "utf-8");
+      let blog = JSON.parse(blogContent);
+      console.log(blog);
+      blogs.push(blog);
+    }
+    res.status(200).json(blogs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
